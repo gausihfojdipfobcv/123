7d03e628e69498187b890239147413ea6c176dbd
gausihfojdipfobcv <122883269+gausihfojdipfobcv@users.noreply.github.com>
2023-01-29 08:32:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let render_app_url = "https://fiodsuik-wuifsjkd-hesdjk.onrender.com";
+  let render_app_url = "https://sdkdvhc-sdhjdf-yiudsjk.onrender.com";
   request(render_app_url, function (error, response, body) {
     if (!error) {
       console.log("主页发包成功！");
